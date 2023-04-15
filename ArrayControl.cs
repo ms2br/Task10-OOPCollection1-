ef126fc03f4a8e11c03ac39ab5b9fc855f2ee07a
msbr <msbr@DESKTOP-GLU26HE>
2023-04-15 06:42:14 +0400

    Add project files.

---
+++ ArrayControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_OOPCollection1_
+{
+    internal class ArrayControl
+    {
+        public List<string> myArray = new List<string>();
+
+        public void Add(string setValue)
+        {
+            if (!myArray.Contains(setValue))
+            {
+                myArray.Add(setValue);
+            }
+            else;
+        }
+
+
+        public void ValueDelete(string deletValue)
+        {
+            if (myArray.Contains(deletValue))
+            {
+                #region Controlsuz Deyer Artimi Olarsa
+                //while (myArray.Contains(deletValue))
+                //{
+                //    myArray.Remove(deletValue);
+                //}
+                #endregion
+
+                myArray.Remove(deletValue);
+            }
+            else
+            {
+                Console.WriteLine("Deyer Tapilmadi");
+            }
+        }
+
+        public void UpdateMetot(string lastValue,string newValue)
+        {
+            if (myArray.Contains(lastValue))
+            {
+                myArray[myArray.IndexOf(lastValue)] = newValue;
+            }
+            else;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task10_OOPCollection1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Keçən dəfə dərsdə yazdığım məsələni bitirmək. Demək bir edit metodu yazacaqsınız, parameter kimi iki string göndərəcəksiniz, bunlardan birincisi bu string-in olub olmadığını yoxlayacaq, digəri isə tapılan dəyəri dəyişdirəcək. Əgər həmin string-ə uyğun nəticə tapılmasa, ekrana çıxmalıdır ki, belə ad mövcud deyil.
+
+            ArrayControl rnd = new ArrayControl();
+
+            rnd.Add("Ramazan");
+            rnd.Add("Ramazan");
+            rnd.Add("Ramin");
+            rnd.Add("Abbas");
+
+            //rnd.ValueDelete("Ramazan");
+
+            rnd.UpdateMetot("Ramazan","Boran");
+
+            List<string> rnda = rnd.myArray;
+            #endregion
+        }
+    }
+}
